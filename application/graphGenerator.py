72c4574a9be860c643e05c80c3e4143849baeb91
#55532324 <anton.ridderby@gmail.com>
2020-05-20 19:06:17 +0200

    Uppdated the graph generator to support other weather types

--- application/graphGenerator.py
+++ application/graphGenerator.py
@@ -2,14 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-def generateGraph(dataFrame, timeFrame):
+def generateGraph(dataFrame, timeFrame, dataType):
     """
     Takes a given pandas dataframe and converts it to a graph and saves it to the graphs folder.
     dataFrame: Converts the entire given data frame to a graph
     timeFrame: The dates/time to show on the x axies
-    """
-    
 
+    dataType inputs:
+    temperature
+    precipitation
+    wind
+    sight
+    presusre
+    """  
+    
     fig, ax = plt.subplots(figsize=(15,15))
 
     if (timeFrame != ''):
@@ -20,11 +26,10 @@ def generateGraph(dataFrame, timeFrame):
             ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%d'))
             ax.xaxis.grid(True)
             ax.yaxis.grid(True)
-            ax.set_title("Past 3 months in °C")
             ax.xaxis.set_minor_locator(mdates.WeekdayLocator())
             fig.set_size_inches(18, 5)
             fig.autofmt_xdate()
-          
+        
         elif (timeFrame == 'day'):
             fig, ax = plt.subplots()
             ax.plot(dataFrame.index, dataFrame['value'])
@@ -32,15 +37,31 @@ def generateGraph(dataFrame, timeFrame):
             ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:00'))
             ax.xaxis.grid(True)
             ax.yaxis.grid(True)
-            ax.set_title("Past 24h in °C")
             ax.xaxis.set_minor_locator(mdates.HourLocator())
             fig.set_size_inches(8, 5)
             fig.autofmt_xdate()
+
         try:
+            if (dataType == 'temperature'):
+                ax.set_title("Temperature in °C")
+            elif (dataType == 'precipitation'):
+                ax.set_title("precipitation in mm")
+            elif (dataType == 'wind'):
+                ax.set_title("wind in m/s")
+            elif (dataType == 'sight'):
+                ax.set_title("sight in meters")
+            elif (dataType == 'presusre'):
+                ax.set_title("presusre in hectopascal")
+            else:
+                return False
+
             fig.savefig(f"static/image/graphs/{timeFrame}.png", bbox_inches='tight')
             return True
-        except Exception:
-            return False
 
+        except Exception:
+            return False  
+                  
+        else:
+            return False
     else: 
         return False
--- application/main.py
+++ application/main.py
@@ -10,12 +10,9 @@ def index():
     """Return the index page of the website."""
     temperature = getAPIJson("hour", "temperature")
 
-    generateGraph(getAPIJson("day", "temperature"), "day")
+    generateGraph(getAPIJson("day", "temperature"), "day", "temperature")
     
     return render_template('homepage.html', temperature = temperature)
-
-
-
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port='8080', debug=True)
