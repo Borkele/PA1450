61bc3e39c50a03e9dbf1003a23de41bf6a35b943
Borkele <wargh50@gmail.com>
2020-05-22 19:30:59 +0200

    restored timeframe.py

--- application/timeFrame.py
+++ application/timeFrame.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import datetime
+from dateutil.parser import parse
 
 def trimTimeFrame(dataFrame, startDate, endDate):
     """
     Returns a cropped data frame to between the given dates in the format of:
     YYYY-MM-DD hh:mm:ss
     """
-    return dataFrame[startDate, endDate]
+    return dataFrame[startDate : endDate]
+
+
+def translateDateFormat(inputDate):
+    """ Translates a date into the YYYY-MM-DD format."""
+    original = parse(inputDate)
+    return original.strftime('%Y-%m-%d')
